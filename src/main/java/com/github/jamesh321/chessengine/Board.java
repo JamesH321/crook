001c82d18e0fdc5adbba9115fcbe1f28a7b0a845
JamesH321 <james.hickson321@gmail.com>
2025-06-07 15:33:26 +0100

    Added piece movement

--- src/main/java/com/github/jamesh321/chessengine/Board.java
+++ src/main/java/com/github/jamesh321/chessengine/Board.java
@@ -1,170 +1,58 @@
 package com.github.jamesh321.chessengine;
 
 public class Board {
-    long whitePawns;
-    long whiteKnights;
-    long whiteBishops;
-    long whiteRooks;
-    long whiteQueens;
-    long whiteKings;
-    long blackPawns;
-    long blackKnights;
-    long blackBishops;
-    long blackRooks;
-    long blackQueens;
-    long blackKings;
+    private long[] bitboards = new long[12]; // 0-5 for white pieces, 6-11 for black pieces
 
-    boolean whiteTurn;
-    int castlingRights;
-    int enPassantSquare;
+    private boolean whiteTurn;
+    private int castlingRights;
+    private int enPassantSquare;
 
     public Board() {
-        whitePawns = 0x000000000000FF00L;
-        whiteKnights = 0x0000000000000042L;
-        whiteBishops = 0x0000000000000024L;
-        whiteRooks = 0x0000000000000081L;
-        whiteQueens = 0x0000000000000010L;
-        whiteKings = 0x0000000000000008L;
+        // White pieces (indices 0-5)
+        bitboards[0] = 0x000000000000FF00L; // Pawns
+        bitboards[1] = 0x0000000000000042L; // Knights
+        bitboards[2] = 0x0000000000000024L; // Bishops
+        bitboards[3] = 0x0000000000000081L; // Rooks
+        bitboards[4] = 0x0000000000000010L; // Queens
+        bitboards[5] = 0x0000000000000008L; // Kings
 
-        blackPawns = 0x00FF000000000000L;
-        blackKnights = 0x4200000000000000L;
-        blackBishops = 0x2400000000000000L;
-        blackRooks = 0x8100000000000000L;
-        blackQueens = 0x1000000000000000L;
-        blackKings = 0x0800000000000000L;
+        // Black pieces (indices 6-11)
+        bitboards[6] = 0x00FF000000000000L; // Pawns
+        bitboards[7] = 0x4200000000000000L; // Knights
+        bitboards[8] = 0x2400000000000000L; // Bishops
+        bitboards[9] = 0x8100000000000000L; // Rooks
+        bitboards[10] = 0x1000000000000000L; // Queens
+        bitboards[11] = 0x0800000000000000L; // Kings
 
         whiteTurn = true;
         castlingRights = 0b1111; // BQ, BK, WQ, WK
         enPassantSquare = -1;
     }
 
-    public long getWhitePawns() {
-        return whitePawns;
+    public long[] getBitboards() {
+        return bitboards;
     }
 
-    public void setWhitePawns(long whitePawns) {
-        this.whitePawns = whitePawns;
+    public int getPieceAtSquare(int square) {
+        long pieceBit = 0x8000000000000000L >>> square;
+        for (int i = 0; i < 12; i++) {
+            if ((pieceBit & bitboards[i]) != 0) {
+                return i;
+            }
+        }
+        // Empty square
+        return -1;
     }
 
-    public long getWhiteKnights() {
-        return whiteKnights;
+    public void movePiece(int fromSquare, int toSquare) {
+        long fromBit = 0x8000000000000000L >>> fromSquare;
+        long toBit = 0x8000000000000000L >>> toSquare;
+        int fromPiece = getPieceAtSquare(fromSquare);
+        int toPiece = getPieceAtSquare(toSquare);
+        bitboards[fromPiece] = (bitboards[fromPiece] & ~fromBit) | toBit;
+        if (toPiece > -1) {
+            bitboards[toPiece] &= ~toBit;
+        }
     }
 
-    public void setWhiteKnights(long whiteKnights) {
-        this.whiteKnights = whiteKnights;
-    }
-
-    public long getWhiteBishops() {
-        return whiteBishops;
-    }
-
-    public void setWhiteBishops(long whiteBishops) {
-        this.whiteBishops = whiteBishops;
-    }
-
-    public long getWhiteRooks() {
-        return whiteRooks;
-    }
-
-    public void setWhiteRooks(long whiteRooks) {
-        this.whiteRooks = whiteRooks;
-    }
-
-    public long getWhiteQueens() {
-        return whiteQueens;
-    }
-
-    public void setWhiteQueens(long whiteQueens) {
-        this.whiteQueens = whiteQueens;
-    }
-
-    public long getWhiteKings() {
-        return whiteKings;
-    }
-
-    public void setWhiteKings(long whiteKings) {
-        this.whiteKings = whiteKings;
-    }
-
-    public long getBlackPawns() {
-        return blackPawns;
-    }
-
-    public void setBlackPawns(long blackPawns) {
-        this.blackPawns = blackPawns;
-    }
-
-    public long getBlackKnights() {
-        return blackKnights;
-    }
-
-    public void setBlackKnights(long blackKnights) {
-        this.blackKnights = blackKnights;
-    }
-
-    public long getBlackBishops() {
-        return blackBishops;
-    }
-
-    public void setBlackBishops(long blackBishops) {
-        this.blackBishops = blackBishops;
-    }
-
-    public long getBlackRooks() {
-        return blackRooks;
-    }
-
-    public void setBlackRooks(long blackRooks) {
-        this.blackRooks = blackRooks;
-    }
-
-    public long getBlackQueens() {
-        return blackQueens;
-    }
-
-    public void setBlackQueens(long blackQueens) {
-        this.blackQueens = blackQueens;
-    }
-
-    public long getBlackKings() {
-        return blackKings;
-    }
-
-    public void setBlackKings(long blackKings) {
-        this.blackKings = blackKings;
-    }
-
-    public boolean isWhiteTurn() {
-        return whiteTurn;
-    }
-
-    public void setWhiteTurn(boolean whiteTurn) {
-        this.whiteTurn = whiteTurn;
-    }
-
-    public int getCastlingRights() {
-        return castlingRights;
-    }
-
-    public void setCastlingRights(int castlingRights) {
-        this.castlingRights = castlingRights;
-    }
-
-    public int getEnPassantSquare() {
-        return enPassantSquare;
-    }
-
-    public void setEnPassantSquare(int enPassantSquare) {
-        this.enPassantSquare = enPassantSquare;
-    }
-
-    public long[] getAllPieces() {
-        return new long[] {
-                whitePawns, whiteKnights, whiteBishops, whiteRooks, whiteQueens, whiteKings,
-                blackPawns, blackKnights, blackBishops, blackRooks, blackQueens, blackKings
-        };
-    }
-
-    public char getPieceAtSquare(int square) {
-        
-    }
+}
