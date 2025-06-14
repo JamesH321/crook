fb549d2fc1767653b81d924ca0023fcb7e918e94
JamesH321 <james.hickson321@gmail.com>
2025-06-14 18:41:23 +0100

    Create class for move execution

---
+++ src/main/java/com/github/jamesh321/chessengine/MoveExecutor.java
@@ -0,0 +1,109 @@
+package com.github.jamesh321.chessengine;
+
+public class MoveExecutor {
+    public static void makeMove(Board board, int move) {
+        int from = Move.getFrom(move);
+        int to = Move.getTo(move);
+        long fromMask = 0x8000000000000000L >>> from;
+        long toMask = 0x8000000000000000L >>> to;
+        int fromPiece = board.getPieceAtSquare(from);
+        int toPiece = board.getPieceAtSquare(to);
+
+        switch (Move.getSpecialMove(move)) {
+            case 0: // None
+                movePiece(board, fromPiece, fromMask, toMask);
+                takePiece(board, toPiece, toMask);
+                break;
+            case 1: // Piece promotion
+                int promotionPiece = getPieceIndex(Move.getPromotionPiece(move), board.getWhiteTurn());
+                movePiece(board, promotionPiece, fromMask, toMask);
+                takePiece(board, toPiece, toMask);
+                takePiece(board, fromPiece, fromMask);
+                System.out.println(board.formatBitboard(0));
+                break;
+            case 2: // En passant
+                movePiece(board, fromPiece, fromMask, toMask);
+                takeEnPassantPiece(board, toMask);
+            case 3: // Castling
+                castle(board, toPiece, fromPiece, fromMask, toMask);
+        }
+
+        setCastlingRights(board, fromPiece, from);
+        setEnPassantSquare(board, fromPiece, from, to);
+    }
+
+    public static void movePiece(Board board, int fromPiece, long fromMask, long toMask) {
+        long newBitboard = (board.getBitboard(fromPiece) & ~fromMask) | toMask;
+        board.setBitboard(fromPiece, newBitboard);
+    }
+
+    public static void takePiece(Board board, int toPiece, long toMask) {
+        if (toPiece > -1) {
+            long newBitboard = board.getBitboard(toPiece) & ~toMask;
+            board.setBitboard(toPiece, newBitboard);
+        }
+    }
+
+    public static int getPieceIndex(int promotionPiece, boolean whiteTurn) {
+        int pieceIndex = 4 - promotionPiece;
+        if (!whiteTurn) {
+            pieceIndex += 6;
+        }
+        return pieceIndex;
+    }
+
+    public static void takeEnPassantPiece(Board board, long toMask) {
+        int toPiece;
+        System.out.println(Long.toBinaryString(toMask));
+        if (board.getWhiteTurn()) {
+            toPiece = 6;
+            toMask >>>= 8;
+            System.out.println(Long.toBinaryString(toMask));
+        } else {
+            toPiece = 0;
+            toMask <<= 8;
+        }
+        takePiece(board, toPiece, toMask);
+    }
+
+    public static void castle(Board board, int to, int fromPiece, long fromMask, long toMask) {
+        int rook = 3;
+        if (!board.getWhiteTurn()) {
+            rook = 6;
+        }
+        movePiece(board, fromPiece, fromMask, toMask);
+        // Queenside castle
+        if (to % 8 == 2) {
+            movePiece(board, rook, fromMask << 4, toMask >>> 1);
+        } else {
+            movePiece(board, rook, fromMask >>> 3, toMask << 1);
+        }
+    }
+
+    public static void setEnPassantSquare(Board board, int fromPiece, int from, int to) {
+        if (fromPiece == 0 || fromPiece == 6) {
+            if (from - to == 16) {
+                board.setEnPassantSquare(to + 8);
+            } else if (from - to == -16) {
+                board.setEnPassantSquare(to - 8);
+            }
+        }
+    }
+
+    public static void setCastlingRights(Board board, int fromPiece, int from) {
+        int castlingRights = board.getCastlingRights();
+        switch (fromPiece) {
+            case 3:
+            case 9:
+                castlingRights &= from / 8 == 0 ? ~(fromPiece - 1) : ~((fromPiece - 1) >> 1);
+                break;
+            case 5:
+                castlingRights &= ~0b0011;
+                break;
+            case 11:
+                castlingRights &= ~0b1100;
+                break;
+        }
+        board.setCastlingRights(castlingRights);
+    }
+}
