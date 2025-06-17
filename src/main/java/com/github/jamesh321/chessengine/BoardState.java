8b30e2bbef0b26454566733abe19088d51361b92
JamesH321 <james.hickson321@gmail.com>
2025-06-17 17:35:19 +0100

    Add getters and make attributes private

--- src/main/java/com/github/jamesh321/chessengine/BoardState.java
+++ src/main/java/com/github/jamesh321/chessengine/BoardState.java
@@ -1,15 +1,31 @@
 package com.github.jamesh321.chessengine;
 
 public class BoardState {
-    long[] bitboards;
-    boolean whiteTurn;
-    int castlingRights;
-    int enPassantSquare;
+    private long[] bitboards;
+    private boolean whiteTurn;
+    private int castlingRights;
+    private int enPassantSquare;
 
     BoardState(Board board) {
         this.bitboards = board.getBitboards().clone();
         this.whiteTurn = board.isWhiteTurn();
         this.castlingRights = board.getCastlingRights();
         this.enPassantSquare = board.getEnPassantSquare();
+    }
+
+    public long[] getBitboards() {
+        return bitboards;
+    }
+
+    public boolean isWhiteTurn() {
+        return whiteTurn;
+    }
+
+    public int getCastlingRights() {
+        return castlingRights;
+    }
+
+    public int getEnPassantSquare() {
+        return enPassantSquare;
     }
 }
