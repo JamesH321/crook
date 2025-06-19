4c0765e855d9c02065957c5a1172f006dcb4fd7c
JamesH321 <james.hickson321@gmail.com>
2025-06-19 21:34:24 +0100

    Fix issue with getting pawn capture moves and make some methods private

--- src/main/java/com/github/jamesh321/chessengine/MoveGenerator.java
+++ src/main/java/com/github/jamesh321/chessengine/MoveGenerator.java
@@ -19,58 +19,6 @@ public class MoveGenerator {
         moveList.addAll(generateWhitePawnEnPassantMoves(pawns, emptyNoPromotion, blackPiecesNoPromotion, fileMask,
                 enPassantSquare));
         moveList.addAll(generateWhitePawnPromotionMoves(pawns, emptyPromotion, blackPiecesPromotion, fileMask));
-
-        return moveList;
-    }
-
-    public static ArrayList<Move> generateWhiteSinglePawnPushMoves(long pawns, long emptyNoPromotion) {
-        ArrayList<Move> moveList = new ArrayList<>();
-        long singlePushMoves = (pawns << 8) & emptyNoPromotion;
-        moveList.addAll(getPawnMoveList(singlePushMoves, 8, Move.NORMAL));
-        return moveList;
-    }
-
-    public static ArrayList<Move> generateWhiteDoublePawnPushMoves(long pawns, long emptyNoPromotion) {
-        ArrayList<Move> moveList = new ArrayList<>();
-        long bothSquaresFree = (emptyNoPromotion >>> 16) & (emptyNoPromotion >>> 24) & 0b11111111;
-        long doublePushMoves = (pawns << 16) & (bothSquaresFree << 24);
-        moveList.addAll(getPawnMoveList(doublePushMoves, 16, Move.NORMAL));
-        return moveList;
-    }
-
-    public static ArrayList<Move> generateWhitePawnCaptureMoves(long pawns, long emptyNoPromotion,
-            long blackPiecesNoPromotion,
-            long fileMask) {
-        ArrayList<Move> moveList = new ArrayList<>();
-        long leftCaptureMoves = (pawns << 9) & (blackPiecesNoPromotion & ~fileMask);
-        long rightCaptureMoves = (pawns << 7) & (blackPiecesNoPromotion & ~(fileMask << 8));
-        moveList.addAll(getPawnMoveList(leftCaptureMoves, 9, Move.NORMAL));
-        moveList.addAll(getPawnMoveList(rightCaptureMoves, 7, Move.NORMAL));
-        return moveList;
-    }
-
-    public static ArrayList<Move> generateWhitePawnEnPassantMoves(long pawns, long emptyNoPromotion,
-            long blackPiecesNoPromotion,
-            long fileMask, long enPassantSquare) {
-        ArrayList<Move> moveList = new ArrayList<>();
-        long leftEnPassantMoves = (pawns << 9) & (enPassantSquare & ~fileMask);
-        long rightEnPassantMoves = (pawns << 7) & (enPassantSquare & ~(fileMask << 8));
-        moveList.addAll(getPawnMoveList(leftEnPassantMoves, 9, Move.EN_PASSANT));
-        moveList.addAll(getPawnMoveList(rightEnPassantMoves, 7, Move.EN_PASSANT));
-        return moveList;
-    }
-
-    public static ArrayList<Move> generateWhitePawnPromotionMoves(long pawns, long emptyPromotion,
-            long blackPiecesPromotion, long fileMask) {
-        ArrayList<Move> moveList = new ArrayList<>();
-        long singlePushMoves = (pawns << 8) & emptyPromotion;
-        long leftCaptureMoves = (pawns << 9) & (blackPiecesPromotion & ~fileMask);
-        long rightCaptureMoves = (pawns << 7) & (blackPiecesPromotion & ~(fileMask << 8));
-        for (int i = 0; i < 4; i++) {
-            moveList.addAll(getPawnMoveList(singlePushMoves, 8, 1 + (4 * i)));
-            moveList.addAll(getPawnMoveList(leftCaptureMoves, 9, 1 + (4 * i)));
-            moveList.addAll(getPawnMoveList(rightCaptureMoves, 7, 1 + (4 * i)));
-        }
         return moveList;
     }
 
@@ -79,8 +27,8 @@ public class MoveGenerator {
         long pawns = board.getBitboard(6);
         long emptyNoPromotion = board.getEmptySquares() & ~0b11111111;
         long emptyPromotion = board.getEmptySquares() & 0b11111111;
-        long whitePiecesNoPromotion = board.getBlackPieces() & ~0b11111111;
-        long whitePiecesPromotion = board.getBlackPieces() & 0b11111111;
+        long whitePiecesNoPromotion = board.getWhitePieces() & ~0b11111111;
+        long whitePiecesPromotion = board.getWhitePieces() & 0b11111111;
         long fileMask = 0x101010101010101L;
         long enPassantSquare = 1L << (63 - board.getEnPassantSquare());
 
@@ -90,62 +38,112 @@ public class MoveGenerator {
         moveList.addAll(generateBlackPawnEnPassantMoves(pawns, emptyNoPromotion, whitePiecesNoPromotion, fileMask,
                 enPassantSquare));
         moveList.addAll(generateBlackPawnPromotionMoves(pawns, emptyPromotion, whitePiecesPromotion, fileMask));
-
         return moveList;
     }
 
-    public static ArrayList<Move> generateBlackSinglePawnPushMoves(long pawns, long emptyNoPromotion) {
+    private static ArrayList<Move> generateWhiteSinglePawnPushMoves(long pawns, long emptyNoPromotion) {
         ArrayList<Move> moveList = new ArrayList<>();
-        long singlePushMoves = (pawns >>> 8) & emptyNoPromotion;
-        moveList.addAll(getPawnMoveList(singlePushMoves, -8, Move.NORMAL));
+        long singlePushMoves = (pawns << 8) & emptyNoPromotion;
+        moveList.addAll(getMoveList(singlePushMoves, 8, Move.NORMAL));
         return moveList;
     }
 
-    public static ArrayList<Move> generateBlackDoublePawnPushMoves(long pawns, long emptyNoPromotion) {
+    private static ArrayList<Move> generateWhiteDoublePawnPushMoves(long pawns, long emptyNoPromotion) {
         ArrayList<Move> moveList = new ArrayList<>();
-        long bothSquaresFree = (emptyNoPromotion >>> 40) & (emptyNoPromotion >>> 32) & 0b11111111;
-        long doublePushMoves = (pawns >>> 16) & (bothSquaresFree << 32);
-        moveList.addAll(getPawnMoveList(doublePushMoves, -16, Move.NORMAL));
+        long bothSquaresFree = (emptyNoPromotion >>> 16) & (emptyNoPromotion >>> 24) & 0b11111111;
+        long doublePushMoves = (pawns << 16) & (bothSquaresFree << 24);
+        moveList.addAll(getMoveList(doublePushMoves, 16, Move.NORMAL));
         return moveList;
     }
 
-    public static ArrayList<Move> generateBlackPawnCaptureMoves(long pawns, long emptyNoPromotion,
+    private static ArrayList<Move> generateWhitePawnCaptureMoves(long pawns, long emptyNoPromotion,
             long blackPiecesNoPromotion,
             long fileMask) {
         ArrayList<Move> moveList = new ArrayList<>();
-        long leftCaptureMoves = (pawns >>> 7) & (blackPiecesNoPromotion & ~fileMask);
-        long rightCaptureMoves = (pawns >>> 9) & (blackPiecesNoPromotion & ~(fileMask << 8));
-        moveList.addAll(getPawnMoveList(leftCaptureMoves, -7, Move.NORMAL));
-        moveList.addAll(getPawnMoveList(rightCaptureMoves, -9, Move.NORMAL));
+        long leftCaptureMoves = (pawns << 9) & (blackPiecesNoPromotion & ~fileMask);
+        long rightCaptureMoves = (pawns << 7) & (blackPiecesNoPromotion & ~(fileMask << 7));
+        moveList.addAll(getMoveList(leftCaptureMoves, 9, Move.NORMAL));
+        moveList.addAll(getMoveList(rightCaptureMoves, 7, Move.NORMAL));
         return moveList;
     }
 
-    public static ArrayList<Move> generateBlackPawnEnPassantMoves(long pawns, long emptyNoPromotion,
+    private static ArrayList<Move> generateWhitePawnEnPassantMoves(long pawns, long emptyNoPromotion,
             long blackPiecesNoPromotion,
             long fileMask, long enPassantSquare) {
         ArrayList<Move> moveList = new ArrayList<>();
-        long leftEnPassantMoves = (pawns >>> 7) & (enPassantSquare & ~fileMask);
-        long rightEnPassantMoves = (pawns >>> 9) & (enPassantSquare & ~(fileMask << 8));
-        moveList.addAll(getPawnMoveList(leftEnPassantMoves, -7, Move.EN_PASSANT));
-        moveList.addAll(getPawnMoveList(rightEnPassantMoves, -9, Move.EN_PASSANT));
+        long leftEnPassantMoves = (pawns << 9) & (enPassantSquare & ~fileMask);
+        long rightEnPassantMoves = (pawns << 7) & (enPassantSquare & ~(fileMask << 7));
+        moveList.addAll(getMoveList(leftEnPassantMoves, 9, Move.EN_PASSANT));
+        moveList.addAll(getMoveList(rightEnPassantMoves, 7, Move.EN_PASSANT));
         return moveList;
     }
 
-    public static ArrayList<Move> generateBlackPawnPromotionMoves(long pawns, long emptyPromotion,
+    private static ArrayList<Move> generateWhitePawnPromotionMoves(long pawns, long emptyPromotion,
             long blackPiecesPromotion, long fileMask) {
         ArrayList<Move> moveList = new ArrayList<>();
-        long singlePushMoves = (pawns >>> 8) & emptyPromotion;
-        long leftCaptureMoves = (pawns >>> 7) & (blackPiecesPromotion & ~fileMask);
-        long rightCaptureMoves = (pawns >>> 9) & (blackPiecesPromotion & ~(fileMask << 8));
+        long singlePushMoves = (pawns << 8) & emptyPromotion;
+        long leftCaptureMoves = (pawns << 9) & (blackPiecesPromotion & ~fileMask);
+        long rightCaptureMoves = (pawns << 7) & (blackPiecesPromotion & ~(fileMask << 7));
         for (int i = 0; i < 4; i++) {
-            moveList.addAll(getPawnMoveList(singlePushMoves, -8, 1 + (4 * i)));
-            moveList.addAll(getPawnMoveList(leftCaptureMoves, -7, 1 + (4 * i)));
-            moveList.addAll(getPawnMoveList(rightCaptureMoves, -9, 1 + (4 * i)));
+            moveList.addAll(getMoveList(singlePushMoves, 8, 1 + (4 * i)));
+            moveList.addAll(getMoveList(leftCaptureMoves, 9, 1 + (4 * i)));
+            moveList.addAll(getMoveList(rightCaptureMoves, 7, 1 + (4 * i)));
         }
         return moveList;
     }
 
-    public static ArrayList<Move> getPawnMoveList(long moves, int shift, int flag) {
+    private static ArrayList<Move> generateBlackSinglePawnPushMoves(long pawns, long emptyNoPromotion) {
+        ArrayList<Move> moveList = new ArrayList<>();
+        long singlePushMoves = (pawns >>> 8) & emptyNoPromotion;
+        moveList.addAll(getMoveList(singlePushMoves, -8, Move.NORMAL));
+        return moveList;
+    }
+
+    private static ArrayList<Move> generateBlackDoublePawnPushMoves(long pawns, long emptyNoPromotion) {
+        ArrayList<Move> moveList = new ArrayList<>();
+        long bothSquaresFree = (emptyNoPromotion >>> 40) & (emptyNoPromotion >>> 32) & 0b11111111;
+        long doublePushMoves = (pawns >>> 16) & (bothSquaresFree << 32);
+        moveList.addAll(getMoveList(doublePushMoves, -16, Move.NORMAL));
+        return moveList;
+    }
+
+    private static ArrayList<Move> generateBlackPawnCaptureMoves(long pawns, long emptyNoPromotion,
+            long blackPiecesNoPromotion,
+            long fileMask) {
+        ArrayList<Move> moveList = new ArrayList<>();
+        long leftCaptureMoves = (pawns >>> 7) & (blackPiecesNoPromotion & ~fileMask);
+        long rightCaptureMoves = (pawns >>> 9) & (blackPiecesNoPromotion & ~(fileMask << 7));
+        moveList.addAll(getMoveList(leftCaptureMoves, -7, Move.NORMAL));
+        moveList.addAll(getMoveList(rightCaptureMoves, -9, Move.NORMAL));
+        return moveList;
+    }
+
+    private static ArrayList<Move> generateBlackPawnEnPassantMoves(long pawns, long emptyNoPromotion,
+            long blackPiecesNoPromotion,
+            long fileMask, long enPassantSquare) {
+        ArrayList<Move> moveList = new ArrayList<>();
+        long leftEnPassantMoves = (pawns >>> 7) & (enPassantSquare & ~fileMask);
+        long rightEnPassantMoves = (pawns >>> 9) & (enPassantSquare & ~(fileMask << 7));
+        moveList.addAll(getMoveList(leftEnPassantMoves, -7, Move.EN_PASSANT));
+        moveList.addAll(getMoveList(rightEnPassantMoves, -9, Move.EN_PASSANT));
+        return moveList;
+    }
+
+    private static ArrayList<Move> generateBlackPawnPromotionMoves(long pawns, long emptyPromotion,
+            long blackPiecesPromotion, long fileMask) {
+        ArrayList<Move> moveList = new ArrayList<>();
+        long singlePushMoves = (pawns >>> 8) & emptyPromotion;
+        long leftCaptureMoves = (pawns >>> 7) & (blackPiecesPromotion & ~fileMask);
+        long rightCaptureMoves = (pawns >>> 9) & (blackPiecesPromotion & ~(fileMask << 7));
+        for (int i = 0; i < 4; i++) {
+            moveList.addAll(getMoveList(singlePushMoves, -8, 1 + (4 * i)));
+            moveList.addAll(getMoveList(leftCaptureMoves, -7, 1 + (4 * i)));
+            moveList.addAll(getMoveList(rightCaptureMoves, -9, 1 + (4 * i)));
+        }
+        return moveList;
+    }
+
+    private static ArrayList<Move> getMoveList(long moves, int shift, int flag) {
         ArrayList<Move> moveList = new ArrayList<>();
         while (moves != 0) {
             int to = 63 - Long.numberOfTrailingZeros(moves);
