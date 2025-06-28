cbc97056ff66e3838fa5d8e5f6f88abb6d589068
JamesH321 <james.hickson321@gmail.com>
2025-06-29 00:50:43 +0100

    Change board state to include halfmove clock and fullmove counter

--- src/main/java/com/github/jamesh321/chessengine/BoardState.java
+++ src/main/java/com/github/jamesh321/chessengine/BoardState.java
@@ -5,12 +5,16 @@ public class BoardState {
     private boolean whiteTurn;
     private int castlingRights;
     private int enPassantSquare;
+    private int halfmoveClock;
+    private int fullmoveCounter;
 
     BoardState(Board board) {
         this.bitboards = board.getBitboards().clone();
         this.whiteTurn = board.isWhiteTurn();
         this.castlingRights = board.getCastlingRights();
         this.enPassantSquare = board.getEnPassantSquare();
+        this.halfmoveClock = board.getHalfmoveClock();
+        this.fullmoveCounter = board.getFullmoveNumber();
     }
 
     public long[] getBitboards() {
@@ -27,5 +31,13 @@ public class BoardState {
 
     public int getEnPassantSquare() {
         return enPassantSquare;
+    }
+
+    public int getHalfmoveClock() {
+        return halfmoveClock;
+    }
+
+    public int getFullmoveCounter() {
+        return fullmoveCounter;
     }
 }
