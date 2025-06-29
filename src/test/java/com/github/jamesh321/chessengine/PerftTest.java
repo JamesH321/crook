e154cdbb99e67312014c6b49d39a819a07ed7ace
JamesH321 <james.hickson321@gmail.com>
2025-06-29 23:21:55 +0100

    Add test positions and perft divide for debugging

--- src/test/java/com/github/jamesh321/chessengine/PerftTest.java
+++ src/test/java/com/github/jamesh321/chessengine/PerftTest.java
@@ -1,19 +1,39 @@
 package com.github.jamesh321.chessengine;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
 public class PerftTest {
+    Engine engine;
 
+    @BeforeEach
+    void setUp() {
+        engine = new Engine(new Board());
+    }
+
+    // Test the starting position
     @Test
-    void testStartingPositionDepth1() {
-        Engine engine = new Engine(new Board());
-        int depth = 1;
-        long expectedNodes = 20;
+    void testPosition1() {
+        long[] expectedNodes = { 1, 20, 400, 8902, 197281, 4865609 };
+        for (int depth = 0; depth < expectedNodes.length; depth++) {
+            long actualNodes = perft(engine, depth);
+            perftDivide(depth);
+            assertEquals(expectedNodes[depth], actualNodes);
+        }
+    }
 
-        long actualNodes = perft(engine, depth);
-
-        assertEquals(expectedNodes, actualNodes);
+    // Test a position known as kiwipete
+    @Test
+    void testPosition2() {
+        long[] expectedNodes = { 1, 48, 2039, 97862, 4085603 };
+        Fen.load(engine.getBoard(), "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
+        for (int depth = 0; depth < expectedNodes.length; depth++) {
+            long actualNodes = perft(engine, depth);
+            assertEquals(expectedNodes[depth], actualNodes);
+        }
     }
 
     private long perft(Engine engine, int depth) {
@@ -28,5 +48,30 @@ public class PerftTest {
             engine.undoMove();
         }
         return total;
+    }
+
+    private void perftDivide(int depth) {
+        if (depth <= 0) {
+            System.out.println("Depth must be greater than 0 for divide.");
+            return;
+        }
+
+        System.out.println("Running Perft Divide for depth: " + depth);
+        long totalNodes = 0;
+
+        List<Move> moves = MoveGenerator.generateLegalMoves(engine.getBoard());
+
+        for (Move move : moves) {
+            engine.makeMove(move);
+
+            long nodes = perft(engine, depth - 1);
+            totalNodes += nodes;
+
+            System.out.println(move.toString() + ": " + nodes);
+
+            engine.undoMove();
+        }
+
+        System.out.println("\nTotal Nodes: " + totalNodes);
     }
 }
