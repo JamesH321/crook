81d792dd3c311df38e58d3eace9c5c4fb1ebc771
JamesH321 <james.hickson321@gmail.com>
2025-07-16 16:50:03 +0100

    test: Replace magic numbers with named constants

--- src/test/java/com/github/jamesh321/chessengine/MoveExecutorTest.java
+++ src/test/java/com/github/jamesh321/chessengine/MoveExecutorTest.java
@@ -14,7 +14,7 @@ public class MoveExecutorTest {
 
     @Test
     void testNormalMove() {
-        Move move = new Move(55, 39, 0);
+        Move move = new Move(55, 39, Move.NORMAL);
         MoveExecutor.makeMove(board, move);
         assertEquals(0, board.getPieceAtSquare(39));
         assertEquals(-1, board.getPieceAtSquare(55));
@@ -22,18 +22,18 @@ public class MoveExecutorTest {
 
     @Test
     void testTakePiece() {
-        board.setBitboard(6, board.getBitboard(6) | (0x8000000000000000L >>> 36));
-        Move move = new Move(51, 36, 0);
+        board.setBitboard(6, board.getBitboard(6) | (1L >>> 63 - 36));
+        Move move = new Move(51, 36, Move.NORMAL);
         MoveExecutor.makeMove(board, move);
         assertEquals(0, board.getPieceAtSquare(36));
         assertEquals(-1, board.getPieceAtSquare(51));
-        assertFalse((board.getBitboard(6) & (0x8000000000000000L >>> 36)) != 0);
+        assertFalse((board.getBitboard(6) & (1L >>> 63 - 36)) != 0);
     }
 
     @Test
     void testPromotion() {
-        board.setBitboard(0, 0x0100000000000000L >>> 8);
-        Move move = new Move(8, 0, 0b0101);
+        board.setBitboard(0, 1L >>> 63 - 8);
+        Move move = new Move(8, 0, Move.ROOK_PROMOTION);
         MoveExecutor.makeMove(board, move);
         assertEquals(3, board.getPieceAtSquare(0));
         assertEquals(-1, board.getPieceAtSquare(8));
@@ -41,13 +41,13 @@ public class MoveExecutorTest {
 
     @Test
     void testEnPassant() {
-        Move move1 = new Move(55, 31, 0);
+        Move move1 = new Move(55, 31, Move.NORMAL);
         MoveExecutor.makeMove(board, move1);
         board.setWhiteTurn(false);
-        Move move2 = new Move(14, 30, 0);
+        Move move2 = new Move(14, 30, Move.NORMAL);
         MoveExecutor.makeMove(board, move2);
         board.setWhiteTurn(true);
-        Move move3 = new Move(31, 22, 0b0010);
+        Move move3 = new Move(31, 22, Move.EN_PASSANT);
         MoveExecutor.makeMove(board, move3);
         assertEquals(0, board.getPieceAtSquare(22));
         assertEquals(-1, board.getPieceAtSquare(30));
@@ -55,38 +55,38 @@ public class MoveExecutorTest {
 
     @Test
     void testCastlingKingside() {
-        Move move1 = new Move(61, 45, 0);
+        Move move1 = new Move(61, 45, Move.NORMAL);
         MoveExecutor.makeMove(board, move1);
-        Move move2 = new Move(62, 46, 0);
+        Move move2 = new Move(62, 46, Move.NORMAL);
         MoveExecutor.makeMove(board, move2);
-        Move move3 = new Move(60, 62, 0b0011);
+        Move move3 = new Move(60, 62, Move.CASTLE);
         MoveExecutor.makeMove(board, move3);
         assertEquals(5, board.getPieceAtSquare(62));
         assertEquals(3, board.getPieceAtSquare(61));
         assertEquals(-1, board.getPieceAtSquare(60));
         assertEquals(-1, board.getPieceAtSquare(63));
-        assertEquals(0, board.getCastlingRights());
+        assertEquals(0b1100, board.getCastlingRights());
     }
 
     @Test
     void testCastlingQueenside() {
-        Move move1 = new Move(59, 45, 0);
+        Move move1 = new Move(59, 45, Move.NORMAL);
         MoveExecutor.makeMove(board, move1);
         board.setWhiteTurn(true);
-        Move move2 = new Move(58, 46, 0);
+        Move move2 = new Move(58, 46, Move.NORMAL);
         MoveExecutor.makeMove(board, move2);
         board.setWhiteTurn(true);
-        Move move3 = new Move(57, 47, 0);
+        Move move3 = new Move(57, 47, Move.NORMAL);
         MoveExecutor.makeMove(board, move3);
         board.setWhiteTurn(true);
-        Move move4 = new Move(60, 58, 0b0011);
+        Move move4 = new Move(60, 58, Move.CASTLE);
         MoveExecutor.makeMove(board, move4);
         board.setWhiteTurn(true);
         assertEquals(5, board.getPieceAtSquare(58));
         assertEquals(3, board.getPieceAtSquare(59));
         assertEquals(-1, board.getPieceAtSquare(60));
         assertEquals(-1, board.getPieceAtSquare(56));
-        assertEquals(0, board.getCastlingRights());
+        assertEquals(0b1100, board.getCastlingRights());
     }
 
     @Test
