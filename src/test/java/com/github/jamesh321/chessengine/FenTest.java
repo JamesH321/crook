43fb7d3b50d8c596789063f6e2cd36132b149730
JamesH321 <james.hickson321@gmail.com>
2025-07-16 16:48:14 +0100

    fix: Correct parameter order in Fen.load method calls

--- src/test/java/com/github/jamesh321/chessengine/FenTest.java
+++ src/test/java/com/github/jamesh321/chessengine/FenTest.java
@@ -8,7 +8,7 @@ public class FenTest {
     void load_ShouldSetInitialPosition() {
         Board board = new Board();
         String fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-        Fen.load(board, fen);
+        Fen.load(fen, board);
 
         assertEquals(0x000000000000FF00L, board.getBitboard(0));
         assertEquals(0x00FF000000000000L, board.getBitboard(6));
@@ -23,7 +23,7 @@ public class FenTest {
     void load_shouldSetPositionWithEnPassantAndCastling() {
         Board board = new Board();
         String fen = "rnbqkbnr/pppp1ppp/8/4p3/3P4/8/PPP1PPPP/RNBQKBNR b Kq d3 3 7";
-        Fen.load(board, fen);
+        Fen.load(fen, board);
 
         assertFalse(board.isWhiteTurn());
         assertEquals(0b1001, board.getCastlingRights());
@@ -36,7 +36,7 @@ public class FenTest {
     void load_shouldSetPositionNoCastlingNoEnPassant() {
         Board board = new Board();
         String fen = "8/8/8/8/8/8/8/8 w - - 10 20";
-        Fen.load(board, fen);
+        Fen.load(fen, board);
 
         for (int i = 0; i < 12; i++) {
             assertEquals(0L, board.getBitboard(i));
