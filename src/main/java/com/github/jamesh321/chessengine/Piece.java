67a2a9476c3775a5e40a5f43b1c2c136053152b2
JamesH321 <james.hickson321@gmail.com>
2025-07-17 18:34:01 +0100

    feat: Add Piece enum
    This enum represents each chess piece (e.g., White Pawn, Black King) and assigns an int that corresponds to that pieces index in the bitboards array in the board class.

---
+++ src/main/java/com/github/jamesh321/chessengine/Piece.java
@@ -0,0 +1,26 @@
+package com.github.jamesh321.chessengine;
+
+public enum Piece {
+    WHITE_PAWN(0),
+    WHITE_KNIGHT(1),
+    WHITE_BISHOP(2),
+    WHITE_ROOK(3),
+    WHITE_QUEEN(4),
+    WHITE_KING(5),
+    BLACK_PAWN(6),
+    BLACK_KNIGHT(7),
+    BLACK_BISHOP(8),
+    BLACK_ROOK(9),
+    BLACK_QUEEN(10),
+    BLACK_KING(11);
+
+    private final int index;
+
+    Piece(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+}
