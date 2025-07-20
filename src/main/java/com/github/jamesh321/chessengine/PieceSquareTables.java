bd00593d748ecd58cdb64f646305fa753c72b657
JamesH321 <james.hickson321@gmail.com>
2025-07-20 17:22:08 +0100

    feat: Refactor PieceSquareTables to include black piece square tables and initialization method

--- src/main/java/com/github/jamesh321/chessengine/PieceSquareTables.java
+++ src/main/java/com/github/jamesh321/chessengine/PieceSquareTables.java
@@ -1,18 +1,18 @@
 package com.github.jamesh321.chessengine;
 
 public class PieceSquareTables {
-    public static final int[] PAWN_PST = {
-          0,  0,  0,  0,  0,  0,  0,  0,
-         50, 50, 50, 50, 50, 50, 50, 50,
-         10, 10, 20, 30, 30, 20, 10, 10,
-          5,  5, 10, 25, 25, 10,  5,  5,
-          0,  0,  0, 20, 20,  0,  0,  0,
-          5, -5,-10,  0,  0,-10, -5,  5,
-          5, 10, 10,-20,-20, 10, 10,  5,
-          0,  0,  0,  0,  0,  0,  0,  0
+    public static final int[] WHITE_PAWN_PST = {
+         0,  0,  0,  0,  0,  0,  0,  0,
+        50, 50, 50, 50, 50, 50, 50, 50,
+        10, 10, 20, 30, 30, 20, 10, 10,
+         5,  5, 10, 25, 25, 10,  5,  5,
+         0,  0,  0, 20, 20,  0,  0,  0,
+         5, -5,-10,  0,  0,-10, -5,  5,
+         5, 10, 10,-20,-20, 10, 10,  5,
+         0,  0,  0,  0,  0,  0,  0,  0
     };
 
-    public static final int[] KNIGHT_PST = {
+    public static final int[] WHITE_KNIGHT_PST = {
         -50,-40,-30,-30,-30,-30,-40,-50,
         -40,-20,  0,  0,  0,  0,-20,-40,
         -30,  0, 10, 15, 15, 10,  0,-30,
@@ -23,7 +23,7 @@ public class PieceSquareTables {
         -50,-40,-30,-30,-30,-30,-40,-50
     };
 
-    public static final int[] BISHOP_PST = {
+    public static final int[] WHITE_BISHOP_PST = {
         -20,-10,-10,-10,-10,-10,-10,-20,
         -10,  0,  0,  0,  0,  0,  0,-10,
         -10,  0,  5, 10, 10,  5,  0,-10,
@@ -34,18 +34,18 @@ public class PieceSquareTables {
         -20,-10,-10,-10,-10,-10,-10,-20
     };
     
-    public static final int[] ROOK_PST = {
-          0,  0,  0,  0,  0,  0,  0,  0,
-          5, 10, 10, 10, 10, 10, 10,  5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-          0,  0,  0,  5,  5,  0,  0,  0
+    public static final int[] WHITE_ROOK_PST = {
+         0,  0,  0,  0,  0,  0,  0,  0,
+         5, 10, 10, 10, 10, 10, 10,  5,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+         0,  0,  0,  5,  5,  0,  0,  0
     };
     
-    public static final int[] QUEEN_PST = {
+    public static final int[] WHITE_QUEEN_PST = {
         -20,-10,-10, -5, -5,-10,-10,-20,
         -10,  0,  0,  0,  0,  0,  0,-10,
         -10,  0,  5,  5,  5,  5,  0,-10,
@@ -56,7 +56,7 @@ public class PieceSquareTables {
         -20,-10,-10, -5, -5,-10,-10,-20
     };
     
-    public static final int[] KING_MIDDLE_GAME_PST = {
+    public static final int[] WHITE_KING_MIDDLE_GAME_PST = {
         -30,-40,-40,-50,-50,-40,-40,-30,
         -30,-40,-40,-50,-50,-40,-40,-30,
         -30,-40,-40,-50,-50,-40,-40,-30,
@@ -67,7 +67,7 @@ public class PieceSquareTables {
          20, 30, 10,  0,  0, 10, 30, 20
     };
 
-    public static final int[] KING_END_GAME_PST = {
+    public static final int[] WHITE_KING_END_GAME_PST = {
         -50,-40,-30,-20,-20,-30,-40,-50,
         -30,-20,-10,  0,  0,-10,-20,-30,
         -30,-10, 20, 30, 30, 20,-10,-30,
@@ -77,4 +77,34 @@ public class PieceSquareTables {
         -30,-30,  0,  0,  0,  0,-30,-30,
         -50,-30,-30,-30,-30,-30,-30,-50
     };
+
+    public static final int[] BLACK_PAWN_PST = new int[64];
+
+    public static final int[] BLACK_KNIGHT_PST = new int[64];
+
+    public static final int[] BLACK_BISHOP_PST = new int[64];
+
+    public static final int[] BLACK_ROOK_PST = new int[64];
+
+    public static final int[] BLACK_QUEEN_PST = new int[64];
+
+    public static final int[] BLACK_KING_MIDDLE_GAME_PST = new int[64];
+    
+    public static final int[] BLACK_KING_END_GAME_PST = new int[64];
+
+    static {
+        initialiseBlackPST(WHITE_PAWN_PST, BLACK_PAWN_PST);
+        initialiseBlackPST(WHITE_KNIGHT_PST, BLACK_KNIGHT_PST);
+        initialiseBlackPST(WHITE_BISHOP_PST, BLACK_BISHOP_PST);
+        initialiseBlackPST(WHITE_ROOK_PST, BLACK_ROOK_PST);
+        initialiseBlackPST(WHITE_QUEEN_PST, BLACK_QUEEN_PST);
+        initialiseBlackPST(WHITE_KING_MIDDLE_GAME_PST, BLACK_KING_MIDDLE_GAME_PST);
+        initialiseBlackPST(WHITE_KING_END_GAME_PST, BLACK_KING_END_GAME_PST);
+    }
+
+    public static void initialiseBlackPST(int[] whitePST, int[] blackPST) {
+        for (int square = 0; square < 64; square++) {
+            blackPST[square] = whitePST[63 - square];
+        }
+    }
 }
